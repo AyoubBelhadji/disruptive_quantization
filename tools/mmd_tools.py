acf8e2c12277bfa1850d2b69e157d4dbd60f7d4e
Daniel Sharp <dannys4@mit.edu>
2025-01-10 10:57:55 -0500

    adding logdet viz

--- tools/mmd_tools.py
+++ tools/mmd_tools.py
@@ -21,7 +21,7 @@ def compute_mmd(X, Y, kernel):
 @nb.jit()
 def compute_mmd_weighted(X, Y, kernel, weights_X=None, weights_Y=None):
     """
-    Computes the weighted MMD between two sets of samples X and Y.
+    Computes the MMD between two sets of samples X and Y, each of which might have weights.
 
     Parameters:
     - X: numpy array of shape (n, d), first set of samples.
@@ -49,3 +49,44 @@ def compute_mmd_weighted(X, Y, kernel, weights_X=None, weights_Y=None):
         2 * weights_X.T.dot(K_XY).dot(weights_Y)
     )
     return mmd
+
+
+@nb.jit(parallel=True)
+def calculate_MMD_large_weighted_X(X, Y, kernel, weights_Y):
+    """
+    Computes the MMD between two sets of samples X and Y.
+
+    Parameters:
+    - X: numpy array of shape (n, d), first set of samples.
+    - Y: numpy array of shape (m, d), second set of samples.
+    - kernel: a kernel function object with a `kernel` method.
+    - weights_X: numpy array of shape (n,), weights for samples in X.
+
+    Returns:
+    - mmd: the weighted MMD value.
+    """
+    d = X.shape[1]
+    assert d == Y.shape[1], f"Dimension mismatch: {d} != {Y.shape[1]}"
+    N, M = len(X), len(Y)
+    assert M == len(weights_Y), f"Dimension mismatch: {M} != {len(weights_Y)}"
+    x_wt = 1./N
+    N_max = max(N,M)
+    mmd_sq = 0
+    for idx in nb.prange(N_max*N_max):
+        i,j = idx // N_max, idx % N_max
+        add_to_mmd = 0.
+        # Add K_XX
+        if i < N and j <= i:
+            mul = 1 + (i != j)
+            add_to_mmd += mul * kernel(X[i], X[j]) * x_wt * x_wt
+        # Add K_YY
+        if i < M and j <= i:
+            y_wt_i, y_wt_j = weights_Y[i], weights_Y[j]
+            mul = 1 + (i != j)
+            add_to_mmd += mul * kernel(Y[i], Y[j]) * y_wt_i * y_wt_j
+        # Subtract 2K_XY
+        if i < N and j < M:
+            y_wt_i = weights_Y[i]
+            add_to_mmd -= 2 * kernel(X[i], Y[j]) * y_wt_i * x_wt
+        mmd_sq += add_to_mmd
+    return np.sqrt(mmd_sq)
--- tools/visualization_tools.py
+++ tools/visualization_tools.py
@@ -10,7 +10,7 @@ Also also created on Mon Nov 18 6:22:10 2024
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
-from tools.mmd_tools import compute_mmd_weighted
+import tools.mmd_tools as mmd
 import numba as nb
 
 from .files_tools import *
@@ -73,18 +73,23 @@ def compute_all_mmds(all_nodes_arr, Y, kernel, all_weights_arr):
     for i in nb.prange(len(all_nodes)):
         X = all_nodes[i]
         weights_X = all_weights[i]
-        mmds[i] = compute_mmd_weighted(X, Y, kernel, weights_X)
+        mmds[i] = mmd.compute_mmd_weighted(X, Y, kernel, weights_X)
     return mmds.reshape(all_nodes_arr.shape[:-2])
 
-def visualize_and_save_dynamics_with_mmd(alg_name, experiment_name, c_array_trajectory, w_array, data_array, kernel, config_folder = ""):
-    R, T, M, _ = c_array_trajectory.shape
-    mmd_values = np.zeros((R, T))
+@nb.jit(parallel=True)
+def compute_all_kernel_logdets(all_nodes_arr, kernel):
+    M, D = all_nodes_arr.shape[-2:]
+    all_nodes = all_nodes_arr.reshape(-1, M, D)
+    logdets = np.empty(len(all_nodes))
+    for i in nb.prange(len(all_nodes)):
+        X = all_nodes[i]
+        k_XX = mmd.broadcast_kernel(kernel, X, X)
+        logdets[i] = np.linalg.slogdet(k_XX)[1]
+    return logdets.reshape(all_nodes_arr.shape[:-2])
 
-    mmd_folder = os.path.join("figures", config_folder , experiment_name, "plots")
-    os.makedirs(mmd_folder, exist_ok=True)
-
-    mmd_values = compute_all_mmds(c_array_trajectory, data_array, kernel, w_array)
-    w_sums = w_array.sum(axis=2)
+def weight_sum_plot(alg_name, mmd_folder, w_sums):
+    """ Plot the sum of weights over iterations for all repetitions """
+    R, T = w_sums.shape
     plt.figure()
     for r in range(R):
         plt.plot(range(T), w_sums[r], label=f"r={r}", lw=3)
@@ -95,68 +100,68 @@ def visualize_and_save_dynamics_with_mmd(alg_name, experiment_name, c_array_traj
     plt.savefig(os.path.join(mmd_folder, "sum_of_weights.png"))
     plt.show()
 
-    for r in range(R):
-        for m in range(M):
-            plt.figure()
-            plt.plot(range(T), w_array[r, :, m], label=f"m={m}")
-            plt.title(f"Plot of weights, r={r}, {alg_name}")
-            plt.xlabel("t")
-            plt.ylabel("w_array")
-            plt.xscale('log')
-            plt.legend()
-            plt.grid(True)
+def weight_evolution_plot(alg_name, w_array, mmd_folder, r, m):
+    """ Plot the evolution of each weight for a given repetition and centroid """
+    T = w_array.shape[1]
+    plt.figure()
+    plt.plot(range(T), w_array[r, :, m], label=f"m={m}")
+    plt.title(f"Plot of weights, r={r}, {alg_name}")
+    plt.xlabel("t")
+    plt.ylabel("w_array")
+    plt.xscale('log')
+    plt.legend()
+    plt.grid(True)
 
-            w_array_plot_path = os.path.join(mmd_folder, "w_array_evolution_r_"+str(r)+"_m_"+str(m)+".png")
-            plt.savefig(w_array_plot_path)
+    w_array_plot_path = os.path.join(mmd_folder, "w_array_evolution_r_"+str(r)+"_m_"+str(m)+".png")
+    plt.savefig(w_array_plot_path)
+    plt.show()
 
-            plt.show()
-
-    for r in range(R):
-        fig, axes = plt.subplots( M+1,1, figsize=(8, (M+1) * 5))  # 1 row, M+1 columns
-        ax = axes[0]
-        ax.plot(range(T), mmd_values[r], color = "black")
+def mmd_weight_evolution_plot(alg_name, w_array, mmd_values, mmd_folder, r):
+    """ Plot MMD evolution and the value of each weight for a given repetition """
+    T, M = w_array.shape[1:]
+    fig, axes = plt.subplots( M+1,1, figsize=(8, (M+1) * 5))  # 1 row, M+1 columns
+    ax = axes[0]
+    ax.plot(range(T), mmd_values[r], color = "black")
+    ax.set_xscale('log')
+    ax.set_title(f"Evolution of MMD, r={r}, {alg_name}")
+    for m in range(M):
+        ax = axes[m+1]  # Select the current subplot
+        ax.plot(range(T), w_array[r, :, m], label=f"m={m}", color = "black")
+        ax.set_xlabel("t")
         ax.set_xscale('log')
-        ax.set_title(f"Evolution of MMD, r={r}, {alg_name}")
-        for m in range(M):
-            ax = axes[m+1]  # Select the current subplot
-            ax.plot(range(T), w_array[r, :, m], label=f"m={m}", color = "black")
-            ax.set_xlabel("t")
-            ax.set_xscale('log')
-            ax.legend()
-            ax.grid(True)
+        ax.legend()
+        ax.grid(True)
+
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust space for the title
+    w_array_plot_path = os.path.join(mmd_folder, f"w_array_evolution_r_{r}_horizontal.png")
+    plt.savefig(w_array_plot_path)
+    plt.show()
+
+def mmd_weight_signs_plot(alg_name, w_array, mmd_values, mmd_folder, r):
+    """ Plot MMD evolution and the sign of each weight for a given repetition """
+    T, M = w_array.shape[1:]
+    fig, axes = plt.subplots( M+1,1, figsize=(8, (M+1) * 5))  # 1 row, M+1 columns
+    ax = axes[0]
+    ax.plot(range(T), mmd_values[r], label="MMD", color = "black")
+    ax.set_xscale('log')
+    ax.set_title(f"Evolution of MMD with weight signs, r={r}, {alg_name}")
+    for m in range(M):
+        ax = axes[m+1]  # Select the current subplot
+        ax.plot(range(T), np.sign(w_array[r, :, m]), label=f"m={m}", color = "black")
+        ax.set_xlabel("t")
+        ax.set_xscale('log')
+        ax.legend()
+        ax.grid(True)
 
         # Adjust layout and save the figure
-        #fig.suptitle(f"Evolution of w_array for m={m}")
-        plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust space for the title
-        w_array_plot_path = os.path.join(mmd_folder, f"w_array_evolution_r_{r}_horizontal.png")
-        plt.savefig(w_array_plot_path)
-        plt.show()
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust space for the title
+    w_array_plot_path = os.path.join(mmd_folder, f"w_array_evolution_r_{r}_horizontal_sign.png")
+    plt.savefig(w_array_plot_path)
+    plt.show()
 
-    for r in range(R):
-        fig, axes = plt.subplots( M+1,1, figsize=(8, (M+1) * 5))  # 1 row, M+1 columns
-        ax = axes[0]
-        ax.plot(range(T), mmd_values[r], label="MMD", color = "black")
-        ax.set_xscale('log')
-        ax.set_title(f"Evolution of MMD with weight signs, r={r}, {alg_name}")
-        #plt.plot(range(T), mmd_values[r], color='black')
-        for m in range(M):
-            ax = axes[m+1]  # Select the current subplot
-            ax.plot(range(T), np.sign(w_array[r, :, m]), label=f"m={m}", color = "black")
-            #ax.set_title(f"r={r}, m={m}")
-            ax.set_xlabel("t")
-            #ax.set_ylabel("w_array")
-            ax.set_xscale('log')
-            ax.legend()
-            ax.grid(True)
-
-        # Adjust layout and save the figure
-        #fig.suptitle(f"Evolution of w_array for m={m}")
-        plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust space for the title
-        w_array_plot_path = os.path.join(mmd_folder, f"w_array_evolution_r_{r}_horizontal_sign.png")
-        plt.savefig(w_array_plot_path)
-        plt.show()
-
-    # Plot MMD over iterations for all repetitions
+def mmd_all_plot(alg_name, mmd_values, mmd_folder):
+    """ Plot MMD evolution over iterations for all repetitions """
+    R, T = mmd_values.shape
     plt.figure(figsize=(10, 6))
     for r in range(R):
         plt.plot(range(T), mmd_values[r], label=f"r={r}", lw=3)
@@ -171,7 +176,53 @@ def visualize_and_save_dynamics_with_mmd(alg_name, experiment_name, c_array_traj
     mmd_plot_path = os.path.join(mmd_folder, "mmd_evolution.png")
     plt.savefig(mmd_plot_path)
     plt.show()
+
+def logdet_all_plot(alg_name, logdets, mmd_folder):
+    """ Plot logdet evolution over iterations for all repetitions """
+    R, T = logdets.shape
+    plt.figure(figsize=(10, 6))
+    for r in range(R):
+        plt.plot(range(T), logdets[r], label=f"r={r}", lw=3)
+
+    plt.title(f"Logdet evolution over iterations, {alg_name}")
+    plt.xlabel("Iteration")
+    plt.ylabel("Logdet")
+    plt.xscale('log')
+    plt.legend()
+    plt.grid()
+    logdet_plot_path = os.path.join(mmd_folder, "logdet_evolution.png")
+    plt.savefig(logdet_plot_path)
+    plt.show()
+
+def visualize_and_save_dynamics_with_mmd(alg_name, experiment_name, c_array_trajectory, w_array, data_array, kernel, config_folder = ""):
+    R, _, M, _ = c_array_trajectory.shape
+
+    mmd_folder = os.path.join("figures", config_folder , experiment_name, "plots")
+    os.makedirs(mmd_folder, exist_ok=True)
+
+    mmd_values = compute_all_mmds(c_array_trajectory, data_array, kernel, w_array)
+    logdets = compute_all_kernel_logdets(c_array_trajectory, kernel)
+
+    w_sums = w_array.sum(axis=2)
+    # Plot the sum of weights over iterations
+    weight_sum_plot(alg_name, mmd_folder, w_sums)
+
+    for r in range(R):
+        for m in range(M):
+            # Plot the evolution of each weight
+            weight_evolution_plot(alg_name, w_array, mmd_folder, r, m)
+
+    for r in range(R):
+        # Plot MMD evolution and the value of each weight
+        mmd_weight_evolution_plot(alg_name, w_array, mmd_values, mmd_folder, r)
+
+    for r in range(R):
+        # Plot MMD evolution and the sign of each weight
+        mmd_weight_signs_plot(alg_name, w_array, mmd_values, mmd_folder, r)
+
+    # Plot MMD over iterations for all repetitions
+    mmd_all_plot(alg_name, mmd_values, mmd_folder)
+
+    # Plot logdet over iterations for all repetitions
+    logdet_all_plot(alg_name, logdets, mmd_folder)
     plt.close()
-
-    return mmd_plot_path
-

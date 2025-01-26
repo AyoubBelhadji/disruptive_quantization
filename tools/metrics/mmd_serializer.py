c782fb08c34bba3959eeacacd95677cb87387d8f
Daniel Sharp <dannys4@mit.edu>
2025-01-25 21:04:13 -0500

    Add file locking

--- tools/metrics/mmd_serializer.py
+++ tools/metrics/mmd_serializer.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import fcntl
 
 MMD_VALS_FILENAME = "self_mmd_vals.pkl"
 class Self_MMD_Dict():
@@ -12,12 +13,22 @@ class Self_MMD_Dict():
         # Load mmd_vals.pkl if it exists
         if os.path.exists(dataset_path):
             with open(dataset_path, 'rb') as f:
+                fcntl.flock(f.fileno(), fcntl.LOCK_SH)
                 self.mmd_vals = pickle.load(f)
+                fcntl.flock(f.fileno(), fcntl.LOCK_UN)
         else:
             self.mmd_vals = {}
+            open(dataset_path, 'a').close()
+        self.last_modified = os.path.getmtime(self.path)
         self.N = N
 
     def __getitem__(self, kernel):
+        if os.path.getmtime(self.path) > self.last_modified:
+            with open(self.path, 'rb') as f:
+                fcntl.flock(f.fileno(), fcntl.LOCK_SH)
+                self.mmd_vals = pickle.load(f)
+                fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+            self.last_modified = os.path.getmtime(self.path)
         key = (self.N, kernel.get_key())
         return self.mmd_vals.get((self.N, key), None)
 
@@ -27,4 +38,6 @@ class Self_MMD_Dict():
         if key not in self.mmd_vals:
             self.mmd_vals[(self.N,key)] = self_MMD
             with open(self.path, 'wb') as f:
+                fcntl.flock(f.fileno(), fcntl.LOCK_EX)
                 pickle.dump(self.mmd_vals, f)
+                fcntl.flock(f.fileno(), fcntl.LOCK_UN)

b035459a051d605bc315ae687fcfe1f9ac4e89c1
Daniel Sharp <dannys4@mit.edu>
2025-01-28 17:19:22 -0500

    Accelerate IIDMS

--- algorithms/IKBQ/IIDMS/mean_shift.py
+++ algorithms/IKBQ/IIDMS/mean_shift.py
@@ -1,8 +1,29 @@
 # from .sub_algorithm import SubAlgorithm
 from algorithms.IKBQ import IterativeKernelBasedQuantization
 import numpy as np
+import numba as nb
 from tools.utils import proj_simplex, broadcast_kernel, kernel_avg, kernel_bar_moment
 
+@nb.jit()
+def get_weights(kernel, y_array, x_array):
+    K_matrix = broadcast_kernel(kernel, y_array, y_array)
+    mu_array = kernel_avg(kernel, y_array, x_array)
+
+    w_tplus1_tilde = mu_array / np.diag(K_matrix)
+    # w_tplus1 = proj_simplex(w_tplus1_tilde)
+
+    return w_tplus1_tilde
+
+@nb.jit()
+def get_centroids(y_array, x_array, kernel_bar, step_size):
+    v1_bar = kernel_bar_moment(kernel_bar, y_array, x_array)
+    v0_bar = kernel_avg(kernel_bar, y_array, x_array)
+    for i in range(len(v0_bar)):
+        v1_bar[i] = v1_bar[i] / v0_bar[i]
+
+    y_tplus1_array = (1 - step_size) * y_array + step_size * v1_bar
+
+    return y_tplus1_array
 
 class MeanShift(IterativeKernelBasedQuantization):
     # IID Mean shift algorithm
@@ -16,14 +37,7 @@ class MeanShift(IterativeKernelBasedQuantization):
 
         # Be careful because K means kernel matrix and number of centroids
         kernel = self.kernel_scheduler.GetKernel()
-
-        K_matrix = broadcast_kernel(kernel, y_array, y_array)
-        mu_array = kernel_avg(kernel, y_array, x_array)
-
-        w_tplus1_tilde = mu_array / np.diag(K_matrix)
-        w_tplus1 = proj_simplex(w_tplus1_tilde)
-
-        return w_tplus1
+        return get_weights(kernel, y_array, x_array)
 
     def calculate_centroids(self, y_array, t, w_array):
         x_array = self.data_array
@@ -31,10 +45,4 @@ class MeanShift(IterativeKernelBasedQuantization):
         # Get the kernel and prekernel functions
         kernel = self.kernel_scheduler.GetKernelInstance()
 
-        v1_bar = kernel_bar_moment(kernel.kernel_bar, y_array, x_array)
-        v0_bar = kernel_avg(kernel.kernel_bar, y_array, x_array)
-        proposed_yt = v1_bar / v0_bar[:, np.newaxis]
-
-        y_tplus1_array = (1 - self.step_size) * y_array + self.step_size * proposed_yt
-
-        return y_tplus1_array
+        return get_centroids(y_array, x_array, kernel.kernel_bar, self.step_size)

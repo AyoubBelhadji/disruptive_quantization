6efa994aef09eaae2107dc2c9b49ec708b8a94ef
Daniel Sharp <dannys4@mit.edu>
2025-05-21 11:02:51 -0400

    Make 2d heavy tail data

--- datasets/heavy_tail/heavy_tail.py
+++ datasets/heavy_tail/heavy_tail.py
@@ -2,10 +2,11 @@ import pickle
 import numpy as np
 def heavy_tail(N_samp: int):
     radius = (-np.log(np.random.rand(N_samp)))**1.5
-    signs = np.random.choice(a=[-1., 1.], size=(N_samp))
-    return (radius * signs).reshape(-1,1)
+    angle = np.random.rand(N_samp)*2*np.pi
+    return np.column_stack([np.cos(angle)*radius, np.sin(angle)*radius])
 
 if __name__ == '__main__':
-    data = heavy_tail(1000)
+    NN = 1000
+    data = heavy_tail(NN)
     with open('data.pkl', 'wb') as f:
         pickle.dump({'labels': np.zeros(NN,dtype=np.int32), 'data': data}, f)
